fcd89238bedde567ce6768e6866fea84ee307762
bhuvanesproengineer <bhuvanes.proengineer@gmail.com>
2026-07-23 08:36:17 +0530

    Fix the frontend requesting url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://erp-system-zr2w.onrender.com',
         changeOrigin: true,
       },
     },
